d9ac19c84b98185df1c85f2744ca15719bca92cf
Dan Taylor <elavibe@me.com>
2025-05-08 09:34:36 -0400

    minor tweaks

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const navigationButton = document.getElementsByClassName("changePicture");
-const picture = document.getElementById("spaceArt");
-const picture2 = document.getElementById("spaceArt2");
+// const navigationButton = document.getElementsByClassName("changePicture");
+// const picture = document.getElementById("spaceArt");
+// const picture2 = document.getElementById("spaceArt2");
 
-navigationButton.addEventListener("click", (e) => {
-  if (picture === classlist("hidden")) picture.classList.add("hidden");
-  picture2.classList.remove("hidden");
-});
+// navigationButton.addEventListener("click", (e) => {
+//   if (picture === classlist("hidden")) picture.classList.add("hidden");
+//   picture2.classList.remove("hidden");
+// });
+
+const navigationButton = document.querySelector(".changePicture");
+const artWork = document.querySelector("spaceArt");
+
+navigationButton.addEventListener("click"),
+  () => {
+    if (artWork.classlist === "hidden") {
+      artWork.classList.remove("hidden");
+    } else {
+      artWork.classList.add("hidden");
+    }
+  };
