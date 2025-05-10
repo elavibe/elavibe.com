262ea9abd34d6e834fe634b1820eda1399b5105f
Dan Taylor <elavibe@me.com>
2025-05-10 10:05:16 -0400

    added more files

---
+++ script.js
@@ -0,0 +1,16 @@
+// const navigationButton = document.getElementsByClassName("changePicture");
+// const picture = document.getElementById("spaceArt");
+// const picture2 = document.getElementById("spaceArt2");
+
+// navigationButton.addEventListener("click", (e) => {
+//   if (picture === classlist("hidden")) picture.classList.add("hidden");
+//   picture2.classList.remove("hidden");
+// });
+
+const navigationButton = document.querySelector(".changePicture");
+const artWork = document.querySelector("spaceArt");
+
+navigationButton.addEventListener("click"),
+  () => {
+    alert("buttonworks");
+  };
