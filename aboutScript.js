bc3d242c75d481313696beecaad39a6f7d8f925e
Dan Taylor <elavibe@me.com>
2025-05-20 19:24:02 -0400

    updated aboutScript so that the button is located on the artwork and style.css

--- aboutScript.js
+++ aboutScript.js
@@ -1,9 +1,20 @@
 const dan = document.querySelector("#danPic");
 const space = document.querySelector("#spacePic");
-//const spaceBtn = ducument.querySelector("#spaceButton");
-const h1Button = document.querySelector(".mc");
 
-h1Button.addEventListener("click", (e) => {
+const danBtn = document.querySelectorAll("img")[0];
+const spaceBtn = document.querySelectorAll("img")[1];
+//const h1Button = document.querySelector(".mc");
+
+// h1Button.addEventListener("click", () => {
+//   space.classList.toggle("hidden");
+//   dan.classList.toggle("hidden");
+// });
+
+spaceBtn.addEventListener("click", () => {
   space.classList.toggle("hidden");
   dan.classList.toggle("hidden");
+});
+danBtn.addEventListener("click", () => {
+  dan.classList.toggle("hidden");
+  space.classList.toggle("hidden");
 });
